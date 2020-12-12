20617a0b415304a2e4123526fd027a06de99cfeb
Keep Walking <luizhenriqueslacerda@hotmail.com>
2020-12-12 20:17:11 -0300

    teste fotos3

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,41 @@
-import {useState } from 'react';
+import Image from 'next/image'
 
-function Home(){
-    return (
-
-        <div class ="container">
-            <reader>
-
-                <h1>Homei2</h1>
-
-                <img
-                    src="https://github.com/luizhcoelho/GitIndividuosocial/blob/main/Icons/Blogger.svg"
-                    alt="My Image"
-                    width={300}
-                    height={300}
-                />
-            </reader>
-            
-            <Contador />
+function Home() {
+  return (
+    <div>
+        <Image src="/Logo Individuo Social.svg"  alt="me"   width="195"  height="70" />
+        <Image src="/whatsapp.svg"  alt="me"   width="26"  height="26" />
+        <Image src="/facebook.svg"  alt="me"   width="26"  height="26" />
+        <Image src="/instagram.svg"  alt="me"   width="26"  height="26" />
+        <Image src="/youtube.svg"  alt="me"   width="26"  height="26" />
+        <Image src="/Blogger.svg"  alt="me"   width="26"  height="26" />
+        <Image src="/Telefone.svg"  alt="me"   width="26"  height="26" />
+        <Image src="/email.svg"  alt="me"   width="26"  height="26" />
+        <Image src="/Localização.svg"  alt="me"   width="26"  height="26" />
 
 
-        </div>
-
-    
-    )
-    
-}
+        <iframe width="870" height="490" src="https://www.youtube.com/embed/tWrGkMn9lHM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 
 
-function Contador(){
-    const [contador,setContador] = useState(1);
+        <h1>Tube New</h1>
+        <Image src="/Tumb Tube New.jpg"  alt="me"   width="400"  height="250" />
+        <h1>Moveis Individuais</h1>
+        <Image src="/Tumb Moveis Individuais.jpg"  alt="me"   width="400"  height="250" />
+        <h1>Cotidiano do Individuo</h1>
+        <Image src="/Tumb Cotidiano do Individuo.jpg"  alt="me"   width="400"  height="250" />
+        <h1>Cursos para o social</h1>
+        <Image src="/Tumb Cursos para o social.jpg"  alt="me"   width="400"  height="250" />
+        <h1>Global News Later</h1>
+        <Image src="/Tumb News Later.jpg"  alt="me"   width="200"  height="110" />
+        
+        
+        
 
-    function adicionarContador(){
-        setContador(contador + 1);
-            
-    }
 
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
+
+    </div>
     )
 }
+
 export default Home
