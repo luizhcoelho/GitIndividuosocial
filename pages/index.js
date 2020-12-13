4b9d2b393f2091d3b47069ae9aecc06cf1b72050
Keep Walking <luizhenriqueslacerda@hotmail.com>
2020-12-12 21:59:33 -0300

    teste fotos4

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,37 @@ function Home() {
   return (
     <div>
         <Image src="/Logo Individuo Social.svg"  alt="me"   width="195"  height="70" />
-        <Image src="/whatsapp.svg"  alt="me"   width="26"  height="26" />
-        <Image src="/facebook.svg"  alt="me"   width="26"  height="26" />
-        <Image src="/instagram.svg"  alt="me"   width="26"  height="26" />
-        <Image src="/youtube.svg"  alt="me"   width="26"  height="26" />
-        <Image src="/Blogger.svg"  alt="me"   width="26"  height="26" />
-        <Image src="/telefone.svg"  alt="me"   width="26"  height="26" />
-        <Image src="/email.svg"  alt="me"   width="26"  height="26" />
-        <Image src="/Localização.svg"  alt="me"   width="26"  height="26" />
+        
+        <a href="https://chat.whatsapp.com/IVrLKCeb5KUGAULVGvWvCd" target="_blank">
+        <Image src="/whatsapp.svg"  alt="whatsapp Icon"   width="26"  height="26" title="Whatsapp" />
+        </a>
 
+        <a href="https://www.facebook.com/IndividuoSocial" target="_blank">
+        <Image src="/facebook.svg"  alt="facebook Icon"   width="26"  height="26" title="FaceBook" />
+        </a>
+        <a href="https://www.instagram.com/individuosocial/" target="_blank">
+         <Image src="/instagram.svg"  alt="minstagram Icon"   width="26"  height="26" title="Instagram" />
+        </a>
+
+        <a href="https://www.youtube.com/channel/UCnXNCenEARk-Z2lFKeHBASw/featured?view_as=subscriber" target="_blank">
+        <Image src="/youtube.svg"  alt="youtube Icon"   width="26"  height="26" title="YouTube" />
+        </a>
+
+        <a href="https://individuosocialbr.blogspot.com/" target="_blank">
+        <Image src="/Blogger.svg"  alt="Blogger Icon"   width="26"  height="26" title="Blogger"  />
+        </a>
+
+        <a href="https://chat.whatsapp.com/IVrLKCeb5KUGAULVGvWvCd" target="_blank">
+        <Image src="/Telefone.svg"  alt="Telefone Icon"   width="26"  height="26" title="Telefone: (32) 99108-3770"  />
+        </a>
+
+        <a href="individuosocialbr@gmail.com" target="_blank">
+        <Image src="/email.svg"  alt="Email Icon"   width="26l"  height="26" title="E-Mail:" />
+        </a>
+
+        <a href="https://goo.gl/maps/HZqNz8c3doY9gDex7" target="_blank">
+        <Image src="/Localização.svg"  alt="Localização Icon"   width="26"  height="26" title="Localização Google Maps"  />
+        </a>
 
         <iframe width="870" height="490" src="https://www.youtube.com/embed/tWrGkMn9lHM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
